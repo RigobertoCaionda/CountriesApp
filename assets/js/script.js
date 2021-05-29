2ae0f1f01974cb204d3ac13e1ffc4311184ca91d
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-05-29 22:21:38 +0100

    Filtering the countries by region

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,78 @@
 let countryList = document.querySelector('.country-list');
+let searchInput = document.querySelector('#searchInput');
+let countryCategory = document.querySelector('#country-category');
+let selectedCountry;
 async function loadingCountries(){
 	let req = await fetch('https://restcountries.eu/rest/v2/all');
 	let json = await req.json();
 	montarPaises(json);
 }
 
-function montarPaises(lista){
-		lista.map((item, index)=>{
-			let pais = document.querySelector('.country').cloneNode(true);
-			document.querySelector('.country').setAttribute('data-key',index);
-			pais.querySelector('.country-img img').src = lista[index].flag;
-			pais.querySelector('.country-details h3').innerHTML = lista[index].name;
-			pais.querySelectorAll('.country-details p')[0].innerHTML = `Population: ${lista[index].population}`;
-			pais.querySelectorAll('.country-details p')[1].innerHTML = `Region: ${lista[index].region}`;
-			pais.querySelectorAll('.country-details p')[2].innerHTML = `Capital: ${lista[index].capital}`;
-			countryList.append(pais);
-		});
-		console.log(lista);
+async function loadingRegion(){
+	let req = await fetch(`https://restcountries.eu/rest/v2/region/${selectedCountry}`);
+	let json = await req.json();
+	montarRegiao(json);
 }
+function montarRegiao(lista){
+	countryList.innerHTML = '';
+	for(let i in lista){
+		countryList.innerHTML += `
+		<div class='country'>
+			<div class='country-img'>
+				<img src='${lista[i].flag}'>
+			</div>
+		<div class='country-details'>
+			<h3>${lista[i].name}</h3>
+			<p>Population: ${lista[i].population}</p>
+			<p>Region: ${lista[i].region}</p>
+			<p>Capital: ${lista[i].capital}</p>
+			</div>
+		</div>`; 
+		document.querySelectorAll('.country')[i].setAttribute('data-key',i);
+	}
+
+}
+function montarPaises(lista){
+		countryList.innerHTML = '';
+	for(let i in lista){
+		countryList.innerHTML += `
+		<div class='country'>
+			<div class='country-img'>
+				<img src='${lista[i].flag}'>
+			</div>
+		<div class='country-details'>
+			<h3>${lista[i].name}</h3>
+			<p>Population: ${lista[i].population}</p>
+			<p>Region: ${lista[i].region}</p>
+			<p>Capital: ${lista[i].capital}</p>
+			</div>
+		</div>`; 
+		document.querySelectorAll('.country')[i].setAttribute('data-key',i);
+	}
+}
+countryCategory.addEventListener('change',()=>{
+	selectedCountry = countryCategory.options[countryCategory.selectedIndex].value;
+	switch(selectedCountry){
+		case 'All':
+		loadingCountries();
+		break;
+		case 'Africa':
+		loadingRegion();
+		break;
+		case 'Americas':
+		loadingRegion();
+		break;
+		case 'Asia':
+		loadingRegion();
+		break;
+		case 'Europe':
+		loadingRegion();
+		break;
+		case 'Oceania':
+		loadingRegion();
+		break;
+		default:
+		alert('escolheu algo valido!');
+	}
+});
 loadingCountries();
